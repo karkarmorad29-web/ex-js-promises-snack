224b73c82feb0b2ec92b02d966fdf511009fa7b5
morad <KARKARMORAD29@GMAIL.COM>
2026-04-14 17:17:52 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+/**
+ * Recupera il titolo di un postdato il suo ID
+ * @param {number} postId - L'ID del post di cui recuperare il titolo
+ * @returns {Promise<string>} - Una promessa che risolve con il titolo del post
+ */
+
+function getPostTitle(Id) {
+    return fetch(`https://dummyjson.com/posts/${Id}`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Errore nella richiesta: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(data => data.title);
+}
+
+// Esempio di utilizzo
+getPostTitle('./posts/1')
+    .then(title => console.log(`Il titolo del post è: ${title}`))
+    .catch(error => console.error(`Si è verificato un errore: ${error.message}`));
